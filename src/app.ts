94ec64e5b2316d699c0e60d2cd9f7d6c831478ba
jilherme <rosa.guilhermediogo@gmail.com>
2023-12-30 00:07:48 -0300

    prisma fundamentals

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Gui',
+    email: '00579097@ufrgs.br',
+  },
+})
+
+// ORM - Object Relational Mapper
