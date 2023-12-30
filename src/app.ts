35d16256b92e97b658f2d9f27e7ca311a355b752
jilherme <rosa.guilhermediogo@gmail.com>
2023-12-30 20:36:12 -0300

    add: Register controller

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,7 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Gui',
-    email: '00579097@ufrgs.br',
-  },
-})
-
-// ORM - Object Relational Mapper
+app.register(appRoutes)
