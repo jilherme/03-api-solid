9b4795fa40e5478b6d674fed518f47975f9159ac
jilherme <rosa.guilhermediogo@gmail.com>
2023-12-30 20:33:04 -0300

    fix: eslint output errors

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  'settings': {
+    'import/resolver': {
+      'typescript': {
+        'project': './tsconfig.json'
+      }
+    },
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': ['@typescript-eslint', 'unused-imports'],
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'import/order': [
+      'warn',
+      {
+        'groups': [
+          'builtin', // Built-in imports (come from NodeJS native) go first
+          'external', // <- External imports
+          'internal', // <- Absolute imports
+          ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+          'index', // <- index imports
+          'unknown' // <- unknown
+        ],
+        'newlines-between': 'always',
+        'alphabetize': {
+          /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
+          'order': 'asc',
+          /* ignore case. Options: [true, false] */
+          'caseInsensitive': true
+        }
+      }
+    ],
+    'unused-imports/no-unused-imports-ts': 'warn',
+  }, 
+}
+
--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-import eslintConfigPrettier from 'eslint-config-prettier'
-import customConfig from '.eslintrc.js'
-
-export default [
-  customConfig,
-  // !should be the last one
-  eslintConfigPrettier,
-]
