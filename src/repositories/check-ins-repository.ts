911889ae37cf926d16bf316ca26341d920488db3
jilherme <rosa.guilhermediogo@gmail.com>
2024-01-01 12:38:21 -0300

    add: TDD & Mocking

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,4 +3,5 @@ import { CheckIn, Prisma } from '@prisma/client'
 export interface CheckInsRepository {
   // unchecked = don't need to create gym or user, will use gymId and userId
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
