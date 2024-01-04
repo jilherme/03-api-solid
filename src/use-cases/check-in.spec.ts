ea155ba5610d93c1342bd306fc3d043b17cbed49
jilherme <rosa.guilhermediogo@gmail.com>
2024-01-04 02:43:49 -0300

    add: check-in distance validation

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -21,8 +21,8 @@ describe('Check-in Use Case', () => {
       title: 'JavaScript Gym',
       description: '',
       phone: '',
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-29.6689015),
+      longitude: new Decimal(-50.9868857),
     })
 
     vi.useFakeTimers()
@@ -83,5 +83,25 @@ describe('Check-in Use Case', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-27.0747279),
+      longitude: new Decimal(-49.4889672),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -27.2092052,
+        userLongitude: -49.6401091,
+      })
+    ).rejects.toBeInstanceOf(Error)
   })
 })
