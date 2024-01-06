869d8f7342601b9f35a940a7d41b5030a49b5fc1
jilherme <rosa.guilhermediogo@gmail.com>
2024-01-05 22:15:07 -0300

    add: user metrics use-case

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,5 +4,6 @@ export interface CheckInsRepository {
   // unchecked = don't need to create gym or user, will use gymId and userId
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  countByUserId(userId: string): Promise<number>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
